f331c269e49f1e287621d4a2376cdae9d20efe41
chandu chandu <praneethas786@gmail.com>
2022-03-01 13:07:31 +0530

    prac1

---
+++ src/main/java/com/test1/pracGitHub/PracGitHubApplication.java
@@ -0,0 +1,14 @@
+package com.test1.pracGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracGitHubApplication.class, args);
+	}
+//	hello
+
+}
---
+++ src/test/java/com/test1/pracGitHub/PracGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test1.pracGitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
